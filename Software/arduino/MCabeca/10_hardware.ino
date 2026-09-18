8b7c683eb687f72a2020ffc0f6af92a013c65cf9
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2026-09-18 15:07:00 -0300

    refactor(mcabeca): separa hardware sensores protocolo e acoes

---
+++ Software/arduino/MCabeca/10_hardware.ino
@@ -0,0 +1,107 @@
+// Hardware -----------------------------------------------------------------
+
+void Start_Serial()
+{
+  Serial.begin(57600);
+  Versao();
+}
+
+void Start_Servo()
+{
+  CabecaX.attach(PIN_CABECA_X);
+  CabecaY.attach(PIN_CABECA_Y);
+  CabecaX.write(CABECA_X_HOME);
+  CabecaY.write(CABECA_Y_HOME);
+}
+
+void Start_Olhos()
+{
+  pinMode(PIN_OLHOS, OUTPUT);
+  digitalWrite(PIN_OLHOS, LOW);
+}
+
+void Start_Cores()
+{
+  pinMode(PIN_LED_AZUL, OUTPUT);
+  pinMode(PIN_LED_VERDE, OUTPUT);
+  pinMode(PIN_LED_VERMELHO, OUTPUT);
+
+  digitalWrite(PIN_LED_AZUL, LOW);
+  digitalWrite(PIN_LED_VERDE, LOW);
+  digitalWrite(PIN_LED_VERMELHO, LOW);
+}
+
+void Start_Laser()
+{
+  pinMode(PIN_LASER, OUTPUT);
+  LaserOFF();
+}
+
+void LedCabecaAzul(bool flag)
+{
+  digitalWrite(PIN_LED_AZUL, flag ? HIGH : LOW);
+}
+
+void LedCabecaVerde(bool flag)
+{
+  digitalWrite(PIN_LED_VERDE, flag ? HIGH : LOW);
+}
+
+void LedCabecaVermelho(bool flag)
+{
+  digitalWrite(PIN_LED_VERMELHO, flag ? HIGH : LOW);
+}
+
+void LedOlhos(bool flag)
+{
+  digitalWrite(PIN_OLHOS, flag ? HIGH : LOW);
+}
+
+void Olhos(int Luz)
+{
+  digitalWrite(PIN_OLHOS, Luz);
+}
+
+void LaserON()
+{
+  digitalWrite(PIN_LASER, HIGH);
+}
+
+void LaserOFF()
+{
+  digitalWrite(PIN_LASER, LOW);
+}
+
+void Point(int y, int x)
+{
+  y = constrain(y, 0, 180);
+  x = constrain(x, 0, 180);
+
+  CabecaY.write(y);
+  CabecaX.write(x);
+}
+
+void VariaLuz()
+{
+  const unsigned long agora = millis();
+  if (agora - ultimoPassoLuz < INTERVALO_LUZ_MS) return;
+
+  ultimoPassoLuz = agora;
+  contcor++;
+
+  // No codigo anterior '(contcor & 16) == 1' nunca era verdadeiro.
+  if ((contcor & 16) != 0)
+  {
+    LedCabecaAzul(false);
+    LedCabecaVerde(false);
+    LedCabecaVermelho(false);
+    LedOlhos(true);
+  }
+  else
+  {
+    LedOlhos(false);
+    LedCabecaAzul((contcor & 2) != 0);
+    LedCabecaVerde((contcor & 4) != 0);
+    LedCabecaVermelho((contcor & 8) != 0);
+  }
+}
